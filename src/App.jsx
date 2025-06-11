1eb2403d063205b8e154b0c3fc9ca5bb4b7974e2
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-11 17:25:40 -0600

    add Error message

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { fetchDataAPI } from "./services/unsplashAPI";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import { ClipLoader } from "react-spinners";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
 
 function App() {
   const [photos, setPhotos] = useState([]);
@@ -13,11 +14,14 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   //handlers
   const handleQueryChange = (query) => {
     setQuery(query);
     setPage(1);
+    setIsEmpty(false);
+    setPhotos([]);
   };
 
   useEffect(() => {
@@ -29,6 +33,8 @@ function App() {
 
         const { results, total } = await fetchDataAPI(query, page);
         setTotalResults(total);
+
+        if (total === 0) return setIsEmpty(true);
 
         if (page === 1) {
           setPhotos(results);
@@ -54,12 +60,14 @@ function App() {
       {isLoading && <ClipLoader className="loader" />}
 
       {photos.length > 0 && !isLoading && photos.length < totalResults && (
-        <LoadMoreBtn onClick={() => setPage((prev) => prev + 1)} />
+        <LoadMoreBtn onClick={() => setPage(page + 1)} />
       )}
 
       {!isLoading && photos.length > 0 && photos.length >= totalResults && (
         <p className="no_more_results">No more results</p>
       )}
+
+      {isEmpty && <ErrorMessage />}
     </Container>
   );
 }
---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,7 @@
+import s from "./ErrorMessage.module.css";
+
+const ErrorMessage = () => {
+  return <p className={s.error_message}>Sorry! Nothing found</p>;
+};
+
+export default ErrorMessage;
